cbb4201bb3ae5b403e2553fd35d84002ef60d266
sotabek <62295238+sotabek@users.noreply.github.com>
2021-03-25 22:26:48 +0100

    Add files via upload

---
+++ Unary_assignment_relational_logical_operators.java
@@ -0,0 +1,29 @@
+package lecture_week2;
+
+public class Unary_assignment_relational_logical_operators {
+
+	public static void main(String[] args) {
+
+		byte b;
+		int i=120;
+		double d=522.33;
+		float f=71.222f;
+		char c='A';
+		
+		c=(char)i;
+		System.out.println("i = " + i + " c = " + c);
+		
+		i=(int)d;
+		
+		System.out.println("d = " + d + " i = " +i);
+		
+		i=(int)f;
+		System.out.println("f = " + f + " i = " +i);
+
+		b=(byte)d;
+		System.out.println("d = " + d + " b = " +b);
+
+		
+	}
+
+}
