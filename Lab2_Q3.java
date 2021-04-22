630017e6defba266c32c7370d700ab25d54e213c
sotabek <62295238+sotabek@users.noreply.github.com>
2021-04-22 06:43:01 +0200

    Update Lab2_Q3.java

--- Lab2_Q3.java
+++ Lab2_Q3.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Lab2_Q3 {
 
 	public static void main(String[] args) {
+		
+		/*
+		 Write a program that asks the user for a positive nonzero integer value. 
+		 The program should use a loop to get 
+		 the sum of all the integers from 1 up to the number entered. 
+		 */
 
 		Scanner sc = new Scanner (System.in);
 		System.out.print("Enter number bigger than 0:");
@@ -14,7 +20,7 @@ public class Lab2_Q3 {
 		for (int i=1;i<=num;i++) {
 			sum=sum+i;
 		}	
-		System.out.print("Sum of up to " + num + " is: " + sum);
+		System.out.print("Sum up to " + num + " is: " + sum);
 	}
 
 }
