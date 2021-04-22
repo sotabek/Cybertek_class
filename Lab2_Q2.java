b240d6e80b16359eb0c863fe24b0b65c82444325
sotabek <62295238+sotabek@users.noreply.github.com>
2021-04-22 06:41:57 +0200

    Update Lab2_Q2.java

--- Lab2_Q2.java
+++ Lab2_Q2.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Lab2_Q2 {
 
 	public static void main(String[] args) {
+		
+		/*
+		 A bag of cookies holds 40 cookies. 
+		 The calorie information on the bag claims that there are 10 serving in the bag 
+		 and that a serving equals 300 calories. write a program that lets the user enter 
+		 the number of cookies he or she actually ate 
+		 and then reports the number of total calories consumed.
+		 
+		 */
 
 		int bag=40;
 		int serving=10;
